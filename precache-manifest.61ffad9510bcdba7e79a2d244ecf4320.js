3faeb5d190074a87fc45f31ae8404b738352bc2f
gvencadze <humblebumble17@gmail.com>
2020-09-14 22:50:40 +0300

    Updates

---
+++ precache-manifest.61ffad9510bcdba7e79a2d244ecf4320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31237dd93f0c93003e7b805d0f5df282",
+    "url": "/miniAppVulnerable/index.html"
+  },
+  {
+    "revision": "bd0a806fec75c97a81f1",
+    "url": "/miniAppVulnerable/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "bd0a806fec75c97a81f1",
+    "url": "/miniAppVulnerable/static/js/2.41b50553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miniAppVulnerable/static/js/2.41b50553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd81dd0b09f53722dcb8",
+    "url": "/miniAppVulnerable/static/js/main.318ec8b0.chunk.js"
+  },
+  {
+    "revision": "3ac66c1b593f445aee0a",
+    "url": "/miniAppVulnerable/static/js/runtime-main.16a7461e.js"
+  }
+]);
